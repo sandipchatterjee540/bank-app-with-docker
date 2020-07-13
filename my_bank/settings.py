e9e31c31bdbeb88dd4658eef85aebf5d59a1f305
sandipchatterjee540 <58181993+sandipchatterjee540@users.noreply.github.com>
2020-07-13 15:26:54 +0530

    Update settings.py

--- my_bank/settings.py
+++ my_bank/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '96_+c-c4k#-kg!+vph7#e5zd(5-pcnx93hfk!+50tk842l*xhe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.0/24','0.0.0.0','*']
 
 
 # Application definition
